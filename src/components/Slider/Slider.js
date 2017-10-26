2b561fdae2a04d92c4d46d3b87f1a284c0030af1
aaron v cohen <avcohen@gmail.com>
2017-10-26 15:20:01 -0400

    a

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -7,22 +7,31 @@ import cafe from '../Consulting/cafe.png';
 
 class SimpleSlider extends React.Component {
   render() {
-    var settings = {
+    const settings = {
       dots: true,
       infinite: true,
       speed: 500,
       slidesToShow: 1,
-      slidesToScroll: 1
+      slidesToScroll: 1,
     };
     return (
-      <div className={cx(s.root,s["edge--top"], s["edge--bottom--reverse"], s["-dark"])} >
+      <div
+        className={cx(
+          s.root,
+          s['edge--top'],
+          s['edge--bottom--reverse'],
+          s['-dark'],
+        )}
+      >
         <div className={s.sliderContainer}>
-          <img className={s.sliderImg} src={cafe} alt=""/>
+          <img className={s.sliderImg} src={cafe} alt="" />
 
           <h3>Cafe Monstruo</h3>
-          <p className={s.sliderText}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus unde voluptatum, at nam modi sit?</p>
+          <p className={s.sliderText}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus
+            unde voluptatum, at nam modi sit?
+          </p>
           <p className={s.sliderLink}>link</p>
-
         </div>
       </div>
     );
